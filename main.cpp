e6958469882c5b3ee2b9254dc566173a7ef33442
AndrewMattimore <a.mattimore@erasolutions.us>
2025-03-16 10:25:32 -0500

    basic tile generation for a floor complete?

--- main.cpp
+++ main.cpp
@@ -1,23 +1,40 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <vector>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "Terraria Clone");
+    sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "Terraria Clone", sf::Style::Default);
 
     sf::Texture texture;
-    if (!texture.loadFromFile("C:\\Users\\sgtma\\source\\repos\\first SFML project\\Debug\\tileset.png", false, sf::IntRect({ 10, 10 }, { 32, 32 })))
+    if (!texture.loadFromFile("C:\\Users\\sgtma\\source\\repos\\first SFML project\\Debug\\tileset.png"))
     {
         std::cerr << "Failed to load image" << std::endl;
+        return -1;
     }
 
     std::cout << "Image Loaded successfully" << std::endl;
 
-    sf::Sprite sprite(texture);
+    const int tileSize = 32;
+    const int windowWidth = 800;
+    const int numTiles = windowWidth / tileSize;
+    const int bottomY = 600 - tileSize;
+
+
+    std::vector<sf::Sprite> textures;
+
+    for (int i = 0; i < numTiles; i++)
+    {
+        sf::Sprite sprite(texture);
+        sprite.setTextureRect(sf::IntRect({ 7, 3 }, { tileSize, tileSize }));
+        sprite.setPosition({ static_cast<float>(i * tileSize), static_cast<float>(bottomY) });
+        textures.push_back(sprite);
+    }
+
 
     while (window.isOpen())
     {
-        while (const std::optional event = window.pollEvent()) // New event loop format
+        while (const std::optional event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>())
                 window.close();
@@ -25,8 +42,17 @@ int main()
 
         window.clear();
 
+
+        for (const auto& sprite : textures)
+        {
+            window.draw(sprite);
+        }
+
         window.display();
     }
 
     return 0;
 }
+
+
+
